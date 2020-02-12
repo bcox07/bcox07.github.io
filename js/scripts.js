b94162766cb28725eedeea4c3504a2647d13d814
brian <bc06255@georgiasouthern.edu>
2020-02-12 10:54:45 -0500

    responsive updates to cards

--- js/scripts.js
+++ js/scripts.js
@@ -82,6 +82,7 @@ function extendPortfolioCard(card, btn) {
     var extendedCardX;
     var extendedCardY;
     let otherCards = document.getElementsByClassName('portfolio__card');
+    
 
     if (window.innerWidth > 600) {
 
@@ -90,13 +91,16 @@ function extendPortfolioCard(card, btn) {
         portfolioContainer.insertBefore(card, otherCards[0]);
         card.childNodes[1].style.width = '98.25%';
         card.childNodes[1].style.marginLeft = '0.875%';
+        //card.childNodes[1].childNodes[1].style.height = '30%';
+
+        unhideElements(card);
 
         if (window.innerWidth > 1200) {
             extendedCardX = 25;
 
             navStickyContainer.style.height = '400%';
 
-            var id = setInterval(frame, 5);
+            var id = setInterval(frame, 0.5);
 
             function frame() {
 
@@ -105,9 +109,9 @@ function extendPortfolioCard(card, btn) {
                 }
                 else {
                     extendedCardX++;
-                    extendedCardY += 8;
+                    extendedCardY += 7;
                     card.style.width = extendedCardX + "%";
-                    card.style.height = extendedCardY + "px";
+                    //card.style.height = extendedCardY + "px";
                 }
             }
         }
@@ -115,7 +119,7 @@ function extendPortfolioCard(card, btn) {
         else {
             extendedCardX = 50;
 
-            var id = setInterval(frame, 10);
+            var id = setInterval(frame, 0.5);
 
             function frame() {
 
@@ -124,9 +128,9 @@ function extendPortfolioCard(card, btn) {
                 }
                 else {
                     extendedCardX++;
-                    extendedCardY += 8;
+                    extendedCardY += 10;
                     card.style.width = extendedCardX + "%";
-                    card.style.height = extendedCardY + "px";
+                    //card.style.height = extendedCardY + "px";
                 }
             }
         }
@@ -149,19 +153,28 @@ function collapsePortfolioCard(card, btn) {
     var extendedCardX;
     var extendedCardY;
 
+    let cardInfo = card.childNodes[1].childNodes[5];
+    
+
     if (window.innerWidth > 600) {
 
         extendedCardY = card.offsetHeight;
 
         card.childNodes[1].style.width = '95%';
         card.childNodes[1].style.marginLeft = '2.5%';
+        card.childNodes[1].childNodes[1].style.height = '40%';
+
+        hideElements(card);
+
+
+        card.childNodes[1].childNodes[7].style.visibility = 'hidden';
 
         if (window.innerWidth > 1200) {
             extendedCardX = 100;
 
             navStickyContainer.style.height = '300%';
 
-            var id = setInterval(frame, 5);
+            var id = setInterval(frame, 0.5);
 
             function frame() {
 
@@ -170,7 +183,7 @@ function collapsePortfolioCard(card, btn) {
                 }
                 else {
                     extendedCardX--;
-                    extendedCardY -= 8;
+                    extendedCardY -= 7;
                     card.style.width = extendedCardX + "%";
                     card.style.height = extendedCardY + "px";
                 }
@@ -186,7 +199,7 @@ function collapsePortfolioCard(card, btn) {
         else {
             extendedCardX = 100;
 
-            var id = setInterval(frame, 10);
+            var id = setInterval(frame, 0.5);
 
             function frame() {
 
@@ -195,7 +208,7 @@ function collapsePortfolioCard(card, btn) {
                 }
                 else {
                     extendedCardX--;
-                    extendedCardY -= 8;
+                    extendedCardY -= 10;
                     card.style.width = extendedCardX + "%";
                     card.style.height = extendedCardY + "px";
                 }
@@ -208,4 +221,67 @@ function collapsePortfolioCard(card, btn) {
             }
         }
     }
+}
+
+function unhideElements(card) {
+
+    let cardInfo = card.childNodes[1].childNodes[5];
+    let cardRoleHeader = card.childNodes[1].childNodes[9];
+    let cardRole = card.childNodes[1].childNodes[11];
+    let cardDividers = [card.childNodes[1].childNodes[7], card.childNodes[1].childNodes[13]];
+    let thumbContainer = card.childNodes[1].childNodes[15];
+
+    cardInfo.style.fontSize = '24px';
+    cardInfo.style.height = 'auto';
+    cardInfo.style.marginBottom = '20px';
+
+    for (const i in cardDividers){
+        cardDividers[i].style.visibility = 'visible';
+    }
+
+    cardRoleHeader.style.visibility = 'visible';
+    cardRoleHeader.style.height = 'auto';
+    cardRoleHeader.style.marginTop = '20px';
+    cardRoleHeader.style.marginBottom = '10px';
+
+    cardRole.style.visibility = 'visible';
+    cardRole.style.height = 'auto';
+    cardRole.style.marginBottom = '20px';
+
+    thumbContainer.style.visibility = 'visible';
+    thumbContainer.style.minHeight = '150px';
+    thumbContainer.style.marginTop = '20px';
+    thumbContainer.style.marginBottom = '20px';
+}
+
+function hideElements(card) {
+
+    let cardInfo = card.childNodes[1].childNodes[5];
+    let cardRoleHeader = card.childNodes[1].childNodes[9];
+    let cardRole = card.childNodes[1].childNodes[11];
+    let cardDividers = [card.childNodes[1].childNodes[7], card.childNodes[1].childNodes[13]];
+    let thumbContainer = card.childNodes[1].childNodes[15];
+
+    cardInfo.style.fontSize = 'initial';
+    cardInfo.style.height = '28%';
+    cardInfo.style.marginBottom = 'initial';
+
+    for (const i in cardDividers){
+        cardDividers[i].style.visibility = 'hidden';
+    }
+
+    cardRoleHeader.style.visibility = 'hidden';
+    cardRoleHeader.style.height = '0';
+    cardRoleHeader.style.marginTop = 'initial';
+    cardRoleHeader.style.marginBottom = 'initial';
+
+    cardRole.style.visibility = 'hidden';
+    cardRole.style.height = '0';
+    cardRole.style.marginBottom = '0';
+
+    thumbContainer.style.visibility = 'hidden';
+    thumbContainer.style.minHeight = '0';
+    thumbContainer.style.height = '0';
+    thumbContainer.style.marginTop = '0';
+    thumbContainer.style.marginBottom = '0';
 }
