1e7da4d0593a512bb8118c92f628ac61bbf31b9a
brian <bc06255@georgiasouthern.edu>
2020-02-26 22:03:30 -0500

    commit

--- js/scripts.js
+++ js/scripts.js
@@ -48,16 +48,14 @@ function load() {
 function getScrollPosition() {
     let pos = document.body.scrollTop;
     var navbar = document.getElementById("stickyNav");
-    var navbarMobile = document.getElementById("stickyNavMobile");
     if (pos > window.innerHeight + 40 && window.innerWidth > 600) {
         navbar.style.display = "block";
     }
     else if (pos > window.innerHeight + 40) {
         navbarMobile.style.display = "block";
     }
-    else {
+    else if (window.innerWidth > 600){
         navbar.style.display = "none";
-        navbarMobile.style.display = "none";
     }
 }
 
